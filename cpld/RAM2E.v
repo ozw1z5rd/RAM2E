e2e86c32cb9bb22c6f695b53e0b7ce2491878740
Zane Kaminski <zanekaminski@gmail.com>
2020-02-16 22:04:02 -0500

    Timing fixes

--- cpld/RAM2E.v
+++ cpld/RAM2E.v
@@ -6,13 +6,14 @@ module RAM2E(C14M, C14M_2, C7M, Q3, PHI0, PHI1,
 			 DelayIn, DelayOut);
 
 	// Control inputs
-	input C14M, PHI0, PHI1, C7M, Q3;
-	input nPRAS, nPCAS, nWE, nWE80, nEN80, nC07X;
+	input C14M, PHI1, C7M;
+	input nWE, nWE80, nEN80, nC07X;
 
 	// Unused inputs
-	input C14M_2, Q3_2, C3M58, AN3, nCASEN;
+	input C14M_2, Q3_2, C3M58, Q3, PHI0;
+	input nPRAS, nPCAS, nCASEN, AN3;
 
-	// Delay (unused)
+	// Delay
 	input [3:0] DelayIn;
 	output [3:0] DelayOut;
 	assign DelayOut[0] = 0; // RC delay unused
@@ -20,6 +21,7 @@ module RAM2E(C14M, C14M_2, C7M, Q3, PHI0, PHI1,
 	assign DelayOut[2] = DelayIn[1];
 	assign DelayOut[3] = 0; // 3rd delay unused
 	wire EN80 = DelayIn[2]; // 2 * 15 ns delay max
+	reg MDBEN = 0;
 
 	// DRAM control
 	output reg nRAS = 1;
@@ -33,14 +35,19 @@ module RAM2E(C14M, C14M_2, C7M, Q3, PHI0, PHI1,
 	wire [4:0] BAS = {BA[4], BA[4] ? BA[5] : BA[3], BA[2:0]};
 	output reg C073SEL = 0; // Bank register select
 
-	// Data bus routing
+	// Video data bus
+	inout [7:0] VD = VDOE ? VDR[7:0] : 8'bZ;
 	reg [7:0] VDR; // Registered video data
 	wire VDOE = ~PHI1;
-	inout [7:0] VD = VDOE ? VDR[7:0] : 8'bZ; // Video data
-	wire MDOE = EN80 & nWE;
-	inout [7:0] MD = MDOE ? RD[7:0] : 8'bZ; // 6502 data bus
+	
+	// 6502 data bus
+	inout [7:0] MD = MDOE ? MDR[7:0] : 8'bZ;
+	reg [7:0] MDR; // Registered DRAM read data
+	wire MDOE = EN80 & nWE & MDBEN;
+	
+	// DRAM data bus
+	inout [7:0] RD = RDOE ? MD[7:0] : 8'bZ;
 	wire RDOE = EN80 & ~nWE;
-	inout [7:0] RD = RDOE ? MD[7:0] : 8'bZ; // DRAM data bus
 
 	/* State Counters */
 	reg PHI1reg = 0; // Saved PHI1 at last rising clock edge
@@ -61,27 +68,33 @@ module RAM2E(C14M, C14M_2, C7M, Q3, PHI0, PHI1,
 
 		// DRAM RAS
 		nRAS <= ~((PHI1 & ~PHI1reg & PHI0seen) | S==4'h1 | S==4'h2 |
-			S==4'h5 |
-			S==4'h7 | S==4'h8 | S==4'h9 | S==4'hA | S==4'hB);
+			(S==4'h4 & Ref==0) |
+			 S==4'h7 | S==4'h8 | S==4'h9 | S==4'hA);
 
 		// DRAM address multiplexing
-		RA[11:8] =
+		RA[11:8] <=
 			(S==4'h6 | S==4'h7) ? {1'b0, BAS[4], BAS[3], BAS[2]} :
 			(S==4'h8 | S==4'h9 | S==4'hA | S==4'hB) ? {1'b0, 1'b0, BAS[1], BAS[0]} : 4'b0;
 			
 		// DRAM CAS
-		if (S==4'h2 | S==4'h4 | (S==4'h9 & nWE80) | (S==4'hB & ~nWE80)) nCAS <= 0;
-		if (S==4'h0 | S==4'h3 | nPRAS) nCAS <= 1;
+		nCAS <= ~(S==4'h2 | S==4'h3 | (S==4'h4 & Ref==0) |
+			S==4'hA | S==4'hB);
+			
+		// Memory data bus gating
+		MDBEN <= S==4'hA | S==4'hB | S==4'hC | S==4'hD | S==4'hE | S==4'hF;
+		
+		// Latch 80-column video data at end of S3
+		if (S==4'h3) VDR[7:0] <= RD[7:0];
 
 		// Latch bank select at end of S7 and S8
 		if (S==4'h0 | S==4'h1) C073SEL <= 0;
 		if (S==4'h7) C073SEL <= (MA[3:0]==4'h1 | MA[3:0]==4'h3 | MA[3:0]==4'h5 | MA[3:0]==4'h7);
 		if (S==4'h8) C073SEL <= C073SEL & ~nC07X & ~nWE;
 		
-		// Set bank register at end of S13
-		if (S==4'hD & C073SEL) BA[5:0] <= MD[5:0];
+		// Set bank register at end of S10
+		if (S==4'hA & C073SEL) BA[5:0] <= MD[5:0];
 		
-		// Latch 80-column video data at end of S3
-		if (S==4'h3) VDR[7:0] <= RD[7:0];
+		// Latch DRAM read data at end of S11
+		if (S==4'hB) MDR[7:0] <= RD[7:0];
 	end
 endmodule
